zlareb1/ClickHouse

commit 66569b42425ef3eefbd77f71c27089b42e91a920

Fixed tiny error [#CLICKHOUSE-2].

libs/libcommon/src/mremap.cpp
@@ -1,4 +1,3 @@
-
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
@@ -18,6 +17,7 @@ void * mremap(
 
     if (!(flags & MREMAP_MAYMOVE))
     {
+        errno = ENOMEM;
         return nullptr;
     }
 

commit a7ac2a2cfdac8265a3f226dd018ddce95d57ad40

Fix clang error on osx regression from 66569b42425ef3eefbd77f71c27089b42e91a920

libs/libcommon/src/mremap.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <sys/mman.h>
+#include <errno.h>
 #include <common/mremap.h>
 
 #if defined(MREMAP_FIXED)