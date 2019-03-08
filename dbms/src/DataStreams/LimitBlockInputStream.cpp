zlareb1/ClickHouse

commit 11e4552ea9185806107c17a051f552defb7b5a04

Don't update "arrow" on client-side for nothing

dbms/src/IO/Progress.h
@@ -53,11 +53,16 @@ struct Progress
     void writeJSON(WriteBuffer & out) const;
 
     /// Each value separately is changed atomically (but not whole object).
-    void incrementPiecewiseAtomically(const Progress & rhs)
+    bool incrementPiecewiseAtomically(const Progress & rhs)
     {
+        if (!rhs.rows)
+            return false;
+
         rows += rhs.rows;
         bytes += rhs.bytes;
         total_rows += rhs.total_rows;
+
+        return true;
     }
 
     void reset()

.gitignore
@@ -243,3 +243,5 @@ website/package-lock.json
 
 # ccls cache
 /.ccls-cache
+
+/compile_commands.json

dbms/programs/client/Client.cpp
@@ -1031,7 +1031,8 @@ private:
 
         // TODO: get the poll_interval from commandline.
         const auto receive_timeout = connection->getTimeouts().receive_timeout;
-        constexpr size_t default_poll_interval = 1000000, min_poll_interval = 5000; /// in microseconds
+        constexpr size_t default_poll_interval = 1000000; /// in microseconds
+        constexpr size_t min_poll_interval = 5000; /// in microseconds
         const size_t poll_interval
             = std::max(min_poll_interval, std::min<size_t>(receive_timeout.totalMicroseconds(), default_poll_interval));
 
@@ -1333,7 +1334,11 @@ private:
 
     void onProgress(const Progress & value)
     {
-        progress.incrementPiecewiseAtomically(value);
+        if (!progress.incrementPiecewiseAtomically(value))
+        {
+            // Just a keep-alive update.
+            return;
+        }
         if (block_out_stream)
             block_out_stream->onProgress(value);
         writeProgress();

commit b12efd96fa761cd5083690df324a025f0f18e88c

Fix broken progress in 19.3 (regression in 11e4552ea9185806107c17a051f552defb7b5a04) Introduce total_rows for table function `numbers()`.
fixes https://github.com/yandex/ClickHouse/issues/4429
resolves https://github.com/yandex/ClickHouse/issues/4284

dbms/src/DataStreams/LimitBlockInputStream.cpp
@@ -6,9 +6,13 @@
 namespace DB
 {
 
-LimitBlockInputStream::LimitBlockInputStream(const BlockInputStreamPtr & input, UInt64 limit_, UInt64 offset_, bool always_read_till_end_)
+LimitBlockInputStream::LimitBlockInputStream(const BlockInputStreamPtr & input, UInt64 limit_, UInt64 offset_, bool always_read_till_end_, bool use_limit_as_total_rows_approx )
     : limit(limit_), offset(offset_), always_read_till_end(always_read_till_end_)
 {
+    if (use_limit_as_total_rows_approx) {
+        addTotalRowsApprox(static_cast<size_t>(limit));
+    }
+
     children.push_back(input);
 }
 

dbms/src/DataStreams/LimitBlockInputStream.h
@@ -16,8 +16,9 @@ public:
       *  returns an empty block, and this causes the query to be canceled.
       * If always_read_till_end = true - reads all the data to the end, but ignores them. This is necessary in rare cases:
       *  when otherwise, due to the cancellation of the request, we would not have received the data for GROUP BY WITH TOTALS from the remote server.
+      * If use_limit_as_total_rows_approx = true, then addTotalRowsApprox is called to use the limit in progress & stats
       */
-    LimitBlockInputStream(const BlockInputStreamPtr & input, UInt64 limit_, UInt64 offset_, bool always_read_till_end_ = false);
+    LimitBlockInputStream(const BlockInputStreamPtr & input, UInt64 limit_, UInt64 offset_, bool always_read_till_end_ = false, bool use_limit_as_total_rows_approx = false);
 
     String getName() const override { return "Limit"; }
 

dbms/src/IO/Progress.h
@@ -55,14 +55,11 @@ struct Progress
     /// Each value separately is changed atomically (but not whole object).
     bool incrementPiecewiseAtomically(const Progress & rhs)
     {
-        if (!rhs.rows)
-            return false;
-
         rows += rhs.rows;
         bytes += rhs.bytes;
         total_rows += rhs.total_rows;
 
-        return true;
+        return rhs.rows ? true : false;
     }
 
     void reset()

dbms/src/Storages/System/StorageSystemNumbers.cpp
@@ -49,7 +49,6 @@ StorageSystemNumbers::StorageSystemNumbers(const std::string & name_, bool multi
     setColumns(ColumnsDescription({{"number", std::make_shared<DataTypeUInt64>()}}));
 }
 
-
 BlockInputStreams StorageSystemNumbers::read(
     const Names & column_names,
     const SelectQueryInfo &,
@@ -75,7 +74,7 @@ BlockInputStreams StorageSystemNumbers::read(
         res[i] = std::make_shared<NumbersBlockInputStream>(max_block_size, offset + i * max_block_size, num_streams * max_block_size);
 
         if (limit)  /// This formula is how to split 'limit' elements to 'num_streams' chunks almost uniformly.
-            res[i] = std::make_shared<LimitBlockInputStream>(res[i], *limit * (i + 1) / num_streams - *limit * i / num_streams, 0);
+            res[i] = std::make_shared<LimitBlockInputStream>(res[i], *limit * (i + 1) / num_streams - *limit * i / num_streams, 0,  false, true);
     }
 
     return res;

dbms/tests/queries/0_stateless/00416_pocopatch_progress_in_http_headers.reference
@@ -1,3 +1,7 @@
+< X-ClickHouse-Progress: {"read_rows":"0","read_bytes":"0","total_rows":"10"}
+< X-ClickHouse-Progress: {"read_rows":"5","read_bytes":"40","total_rows":"10"}
+< X-ClickHouse-Progress: {"read_rows":"10","read_bytes":"80","total_rows":"10"}
+9
 < X-ClickHouse-Progress: {"read_rows":"1","read_bytes":"8","total_rows":"0"}
 < X-ClickHouse-Progress: {"read_rows":"2","read_bytes":"16","total_rows":"0"}
 < X-ClickHouse-Progress: {"read_rows":"3","read_bytes":"24","total_rows":"0"}

dbms/tests/queries/0_stateless/00416_pocopatch_progress_in_http_headers.sh
@@ -3,6 +3,7 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
+${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}?max_block_size=5&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d 'SELECT max(number) FROM numbers(10)' 2>&1 | grep -E 'Content-Encoding|X-ClickHouse-Progress|^[0-9]'
 # This test will fail with external poco (progress not supported)
 
 ${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}?max_block_size=1&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d 'SELECT number FROM system.numbers LIMIT 10' 2>&1 | grep -E 'Content-Encoding|X-ClickHouse-Progress|^[0-9]'