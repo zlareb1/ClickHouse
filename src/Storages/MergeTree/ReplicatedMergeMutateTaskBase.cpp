zlareb1/ClickHouse

commit c4f23074f6cd1c5d2498bba9d49d9783c1a15575

fix gtest with MemoryWriteBuffer, do not mute exception in ReplicatedMergeMutateTaskBase

src/Storages/MergeTree/ReplicatedMergeMutateTaskBase.cpp
@@ -119,8 +119,10 @@ bool ReplicatedMergeMutateTaskBase::executeStep()
                 }
             }
         }
-
     }
+
+    if (saved_exception)
+        std::rethrow_exception(saved_exception);
 
     return false;
 }

src/Disks/tests/gtest_cascade_and_memory_write_buffer.cpp
@@ -223,6 +223,7 @@ TEST(MemoryWriteBuffer, WriteAndReread)
         {
             MemoryWriteBuffer buf(s - 1);
             EXPECT_THROW(buf.write(data.data(), data.size()), MemoryWriteBuffer::CurrentBufferExhausted);
+            buf.finalize();
         }
     }
 

src/Storages/MergeTree/MergeTreeBackgroundExecutor.cpp
@@ -295,7 +295,7 @@ void MergeTreeBackgroundExecutor<Queue>::routine(TaskRuntimeDataPtr item)
             {
                 ALLOW_ALLOCATIONS_IN_SCOPE;
                 /// An exception context is needed to proper delete write buffers without finalization
-                throw Exception(ErrorCodes::ABORTED, "Storage is about to be deleted. Done task as if it was aborted.");
+                throw Exception(ErrorCodes::ABORTED, "Storage is about to be deleted. Done active task as if it was aborted.");
             }
             catch (...)
             {

commit 3502d700539e9d6a5b3acada401837993977fe68

Do not write retriable errors for Replicated mutate/merge into error log
Fixes: c4f23074f6c ("fix gtest with MemoryWriteBuffer, do not mute exception in ReplicatedMergeMutateTaskBase")
Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

src/Storages/MergeTree/ReplicatedMergeMutateTaskBase.cpp
@@ -69,11 +69,11 @@ bool ReplicatedMergeMutateTaskBase::executeStep()
             else
                 tryLogCurrentException(log, __PRETTY_FUNCTION__);
 
-            /** This exception will be written to the queue element, and it can be looked up using `system.replication_queue` table.
-                 * The thread that performs this action will sleep a few seconds after the exception.
-                 * See `queue.processEntry` function.
-                 */
-            throw;
+            /// This exception will be written to the queue element, and it can be looked up using `system.replication_queue` table.
+            /// The thread that performs this action will sleep a few seconds after the exception.
+            /// See `queue.processEntry` function.
+            if (!retryable_error)
+                throw;
         }
         catch (...)
         {